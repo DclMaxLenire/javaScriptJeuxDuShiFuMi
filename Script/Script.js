d0c16a914d2dc7c12cc14441f2dfbf8796b6df76
MaxLenire <Dcl.maximea@18pixel.fr>
2018-10-25 00:07:04 +0200

    jeuxDuShiFuMi il fonctionne enfin

--- Script/Script.js
+++ Script/Script.js
@@ -5,78 +5,76 @@ var boutonFeuille = document.getElementById("feuille"); // Choisir Feuille //
 
 var boutonCiseaux = document.getElementById("ciseaux"); // Choisir Ciseau //
 
-var choixDeIA  = Math.floor(Math.random() * 3);
+var choixIA;
+
+var choixDuIa;
 
 var choixDuJoueur;
 
+var choixJoueur;
+
 var resultat;
 
-// Faire choisir l'IA //
-function choixIa() {
-}
-if (choixDeIA === 0 ) {
-    choixDeIA = "pierre";
-}
-else if(choixDeIA  === 1) {
-    choixDeIA = "feuille";
-}
-else if(choixDeIA === 2) {
-    choixDeIA = "ciseaux";
-}
-choixIa();
-console.log(choixDeIA);
 // Faire choisir l'utilisateur et affiché sont choix //
+
 boutonPierre.addEventListener("click",
-function(){      
-    choixDuJoueur = "pierre";
-    joueur.innerHTML = "Vous avez choisis <br><br> LA PIERRE"  
-console.log(choixDuJoueur);
+function choixPierre() {  
+choixDuJoueur = "pierre";
+choixJoueur =0;
+    joueur.innerHTML = "Vous avez choisis <br><br> LA PIERRE" 
+        console.log(choixDuJoueur + choixJoueur);
+        comparer();
+       
 });
 boutonFeuille.addEventListener("click",
-function choixFeuille(){      
-     choixDuJoueur = "feuille";
-     joueur.innerHTML = "Vous avez choisis <br><br> LA FEUILLE"
-console.log(choixDuJoueur);
+function choixFeuille() {      
+choixDuJoueur = "feuille";
+choixJoueur =1;
+    joueur.innerHTML = "Vous avez choisis <br><br> LA FEUILLE"
+        console.log(choixDuJoueur + choixJoueur);
+        comparer();
+        
 });
 boutonCiseaux.addEventListener("click",
-function choixCiseaux(){       
-     choixDuJoueur ="ciseaux";
-     joueur.innerHTML = "Vous avez choisis <br><br> LA PAIRE DE CISEAUX"
-console.log(choixDuJoueur);             
+function choixCiseaux() {       
+choixDuJoueur ="ciseaux";
+choixJoueur =2;
+    joueur.innerHTML = "Vous avez choisis <br><br> LA PAIRE DE CISEAUX"
+        console.log(choixDuJoueur + choixJoueur);  
+        comparer(); 
 });
-// Montrer le choix de l'ia //
-
-// Comparé les choix //
-function comparer(choixDuJoueur, choixDeIA) {
-    if (choixDuJoueur === "pierre" && choixDeIA === "feuille") {
-        resultat = "Vous avez perdu";
-        console.log(résultat);
-    } else if (choixDuJoueur === "pierre" && choixDeIA === "ciseaux") {
-        resultat = "Vous avez gagnez";
-        console.log(résultat);
-    } else if (choixDuJoueur === "pierre" && choixDeIA === "pierre") {
-        resultat = "Egalité";
-        console.log(résultat);
-    } else if (choixDuJoueur === "feuille" && choixDeIA === "feuille") {
-        resultat = "Egalité";
-        console.log(résultat);
-    } else if (choixDuJoueur === "feuille" && choixDeIA === "pierre") {
-        resultat = "Vous avez gagnez";
+        // Faire choisir l'IA //
+function choixDeIa() {
+    choixIA = Math.floor(Math.random() * 3);
+}
+        // Faire choisir l'IA et montrer le choix //
+choixDeIa();
+function choixIaResultat() {
+if (choixIA == 0 ) {
+    choixDuIa = "pierre";
+}
+else if(choixIA  === 1) {
+    choixDuIa= "feuille";
+}
+else if(choixIA === 2) {
+    choixDuIa = "ciseaux";
+}
+};
+choixIaResultat();
+console.log(choixDuIa);
+// Comparer les choix //
+function comparer() {
+    if (choixJoueur === choixIA) {
+        console.log("Egalité");
+        
+    }
+    else if ((choixJoueur==0) && (choixIA==2) || (choixJoueur==1) && (choixIA==0) || (choixJoueur==2) && (choixIA==1)){
         console.log("Vous avez gagnez");
-    } else if (choixDuJoueur === "feuille" && choixDeIA === "ciseaux") {
-        resultat = "Vous avez perdus";
-        console.log(résultat);
-    } else if (choixDuJoueur === "ciseaux" && choixDeIA === "pierre") {
-        resultat = "Vous avez perdus";
-        console.log(résultat);
-    } else if (choixDuJoueur === "ciseaux" && choixDeIA === "feuille") {
-        resultat = "Vous avez gagnez";
-        console.log(résultat);
-    } else if (choixDuJoueur === "ciseaux" && choixDeIA === "ciseaux") {
-        resultat = "Egalité";
-        console.log(resultat);
+    } else {
+        console.log("Vous avez perdu");
+
     }
 };
-comparer();
-// Donner le résultat //
-// Relancer le jeu //
+
+        // Donner le résultat //
+        // Relancer le jeu //
